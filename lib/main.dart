580bdbb2274d1ffe231f9b5054684e891979ebdf
farahnework <farahemadne@gmail.com>
2025-05-08 18:07:02 +0300

    3

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:app/presentation/pages/home_page.dart';
+import 'package:app/presentation/pages/sell_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
  
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: HomePage(),
+      home: SellPage(),
     );
   }
 }
--- lib/presentation/pages/sell_page.dart
+++ lib/presentation/pages/sell_page.dart
@@ -1,3 +1,6 @@
+import 'package:app/presentation/widgets/custom_icon_button.dart';
+import 'package:app/shared/app_colors.dart';
+import 'package:app/shared/app_sizes.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -11,6 +14,71 @@ class SellPage extends StatefulWidget {
 class _SellPageState extends State<SellPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            AppBar(),
+            Body(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AppBar extends StatelessWidget {
+  const AppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 80,
+      decoration: BoxDecoration(
+      color: AppColors.lightPurple,
+      boxShadow: [
+      BoxShadow(
+        color: Colors.grey.withAlpha(100),
+        spreadRadius: 2,
+        blurRadius: 10,
+        offset: Offset(0, 1), // Changes position of shadow
+      ),
+    ],
+
+      ),
+      width: MediaQuery.of(context).size.width,
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: AppSizes.screenPadding),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('Tareq Erq'),
+            Row(children: [
+              Text('الوردية الرئيسية'),
+              Text('الوردية الحالية:'),
+            ],),
+            CustomIconButton(text: '', icon: Icons.add),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Column(children: [
+            
+          ],),),
+        Expanded(child: Column(children: [],),),
+      ],
+    );
   }
 }
